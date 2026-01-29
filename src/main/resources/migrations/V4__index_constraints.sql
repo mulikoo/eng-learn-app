e3fe0d539ab65801b7ed0b528e5371084ceea4cd
mulikoo <kasnicolas06@gmail.com>
2026-01-29 11:05:46 +0300

    ELP-5 new changes:new fixes 3

--- src/main/resources/migrations/V4__index_constraints.sql
+++
@@ -1,6 +0,0 @@
--- индексы
-CREATE INDEX idx_word_category_id ON word(category_id);
-CREATE INDEX idx_user_current_category ON users(current_category_id);
-CREATE INDEX idx_user_progress_user_word ON yser_progress(user_ID, word_ID);
-CREATE INDEX idx_user_progress_status ON user_progress(status);
-CREATE INDEX idx_category_name ON category(name);
