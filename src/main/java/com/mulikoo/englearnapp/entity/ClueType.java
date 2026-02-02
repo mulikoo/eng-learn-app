1c04e01289d3e89286d105e075e2e6dd0514bdc1
mulikoo <kasnicolas06@gmail.com>
2026-02-02 14:30:56 +0300

    ELP-6: создание entity по всем таблицам. Добавление enum: заполнение данными и исправление ошибок: добавление геттеров

--- src/main/java/com/mulikoo/englearnapp/entity/ClueType.java
+++ src/main/java/com/mulikoo/englearnapp/entity/ClueType.java
@@ -1,9 +1,12 @@
 package com.mulikoo.englearnapp.entity;
 
+import lombok.Getter;
+
 public enum ClueType {
     HINT("hint"),
     TRANSLATION("translation");
 
+    @Getter
     private final String description;
 
     ClueType(String description) {
--- src/main/java/com/mulikoo/englearnapp/entity/MediaType.java
+++ src/main/java/com/mulikoo/englearnapp/entity/MediaType.java
@@ -1,10 +1,13 @@
 package com.mulikoo.englearnapp.entity;
 
+import lombok.Getter;
+
 public enum MediaType {
     AUDIO("audio"),
     PHOTO("image"),
     ;
 
+    @Getter
     private final String description;
 
     MediaType(String description) {
