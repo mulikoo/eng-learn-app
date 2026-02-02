20394804466ca06a92785e16e547456a7db64c52
mulikoo <kasnicolas06@gmail.com>
2026-02-02 14:30:56 +0300

    ELP-6: создание entity по всем таблицам. Добавление enum: заполнение данными и исправление ошибок: добавление геттеров

--- src/main/java/com/mulikoo/englearnapp/EngLearnAppApplication.java
+++ src/main/java/com/mulikoo/englearnapp/EngLearnAppApplication.java
@@ -1,6 +1,5 @@
 package com.mulikoo.englearnapp;
 
-import com.mulikoo.englearnapp.entity.Category;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
