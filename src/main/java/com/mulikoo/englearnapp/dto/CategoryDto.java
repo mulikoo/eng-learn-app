5433a1288d34a0b47d6d4eafc329eed4bd4df0ea
muliko <kasnicolas06@gmail.com>
2026-02-23 16:13:40 +0300

    ELP-11: добавление swagger, описание DTO и контроллеров для Category

--- src/main/java/com/mulikoo/englearnapp/dto/CategoryDto.java
+++ src/main/java/com/mulikoo/englearnapp/dto/CategoryDto.java
@@ -1,22 +1,30 @@
 package com.mulikoo.englearnapp.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Schema(description = "категория")
 public class CategoryDto {
+    @Schema(description = "uid - уникальный индентификатор", example = "123e4567-e89b-12d3-a456-426614174000")
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private UUID uid;
 
+    @Schema(description = "название категории", example = "бизнес английский")
     private String name;
+
+    @Schema(description = "описание категории", example = "английский для работы в коллективе")
     private String description;
 
+    @Schema(description = "дата создания", example = "2026-02-21")
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime creationDate;
 
+    @Schema(description = "дата изменения", example = "2026-02-22")
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime modificationDate;
 }
