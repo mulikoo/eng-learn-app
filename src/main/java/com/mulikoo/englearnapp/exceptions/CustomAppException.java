8ea7a9572a93ece3ae5a8d08cfb3608435ddba56
muliko <kasnicolas06@gmail.com>
2026-03-03 11:01:57 +0300

    ELP-21: глобальный обработчик исключений

---
+++ src/main/java/com/mulikoo/englearnapp/exceptions/CustomAppException.java
@@ -0,0 +1,7 @@
+package com.mulikoo.englearnapp.exceptions;
+
+public class CustomAppException extends RuntimeException {
+    public CustomAppException(String message) {
+        super(message);
+    }
+}
